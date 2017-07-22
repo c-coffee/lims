ec3df7383849382171156006f8d150ffb1ef8625
c-coffee <4165426@qq.com>
2017-07-22 22:28:14 +0800

    完成基本框架搭建

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Main from '../views/Main.vue'
+import Home from '../views/Home.vue'
+
+Vue.use(Router);
+
+export default new Router({
+    routes:[
+        {
+            path:'/',
+            redirect:'/main'
+        },
+        {
+            path:'/main',
+            component:Main,
+            children:[
+                {
+                    path:'/',
+                    redirect:'/home'
+                },
+                {
+                    path:'/home',
+                    component:Home
+                }
+            ]
+        }
+    ]
+})
---
+++ src/views/Main.vue
@@ -0,0 +1,55 @@
+/**
+* Created by vol on 2017/7/18.
+*/
+<template>
+    <div class="main">
+        <!--左右分栏设置-->
+        <div class="left-column">
+            <vSideBar class="el-sidebar" ></vSideBar>
+        </div>
+        <div class="right-column">
+            <router-view class="mainboard"></router-view>
+        </div>
+
+    </div>
+</template>
+<script>
+
+import vSideBar from './SideBar.vue'
+export default{
+    components:{
+        vSideBar
+    }
+}
+</script>
+<style>
+    html{
+        height:100%;
+    }
+    body{
+        height:100%;
+        width:100%;
+        background-color:#2396EB
+    }
+    .left-column{
+        width:260px;
+        float:left;
+        margin-right:-265px;
+    }
+    .right-column{
+        float:left;
+        width:99%;
+        margin:0 10px;
+    }
+    .el-sidebar{
+        background:#324157;
+        position:fixed;
+        top:0;
+        bottom:0;
+        overflow-x: hidden;
+    }
+    .mainboard{
+        margin:10px;
+        margin-left:268px;
+    }
+</style>
