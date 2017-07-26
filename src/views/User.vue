4bf05fe2a3d65945ff5629b3d49c812d44ef6d59
c-coffee <4165426@qq.com>
2017-07-26 17:13:28 +0800

    2017.7.26 用户管理模块

--- src/views/User.vue
+++ src/views/User.vue
@@ -33,6 +33,92 @@
                     <el-button type="primary" @click="addUser()">确 定</el-button>
             </span>
         </el-dialog>
+        <el-dialog title="查看用户"  :visible.sync="dlg_isShowUserInfoDlg" size="small" :close-on-click-modal="dlg_modelCloseOption">
+            <el-form :model="showUserInfoForm" :label-position="dlg_labelPosition" ref="showUserInfoForm" label-width="80px">
+                <el-form-item label="用户姓名">
+                    <el-input  auto-complete="off" readonly="readonly" v-model="showUserInfoForm.name"></el-input>
+                </el-form-item>
+                <el-form-item label="登录帐号">
+                    <el-input  auto-complete="off" readonly="readonly" v-model="showUserInfoForm.loginName"></el-input>
+                </el-form-item>
+                <el-form-item label="初始密码">
+                    <el-input  auto-complete="off" readonly="readonly" value="123" v-model="showUserInfoForm.pwd"></el-input>
+                </el-form-item>
+                <el-form-item label="联系手机" >
+                    <el-input  auto-complete="off" readonly="readonly" v-model="showUserInfoForm.tel"></el-input>
+                </el-form-item>
+                <el-form-item label="所属部门">
+                    <el-select v-model="showUserInfoForm.dept" placeholder="请选择部门...">
+                        <el-option label="业务室" value="4"></el-option>
+                        <el-option label="财务室" value="5"></el-option>
+                        <el-option label="综合科" value="6"></el-option>
+                        <el-option label="检验一室" value="1"></el-option>
+                        <el-option label="检验二室" value="2"></el-option>
+                        <el-option label="检验三室" value="3"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="用户签名">
+                    <img src="../assets/sign.png" width="150">
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                    <el-button type="primary" @click="dlg_isShowUserInfoDlg=false;">确 定</el-button>
+            </span>
+        </el-dialog>
+        <el-dialog title="修改用户"  :visible.sync="dlg_isShowUpdateUserDlg" size="small" :close-on-click-modal="dlg_modelCloseOption">
+            <el-form :model="updateUserInfoForm" :label-position="dlg_labelPosition" :rules="addUser_rules" ref="updateUserForm" label-width="80px">
+                <el-form-item label="用户姓名" prop="name" class="is-required">
+                    <el-input  auto-complete="off" v-model="updateUserInfoForm.name"></el-input>
+                </el-form-item>
+                <el-form-item label="登录帐号" prop="loginName" class="is-required">
+                    <el-input  auto-complete="off" readonly="readonly" v-model="updateUserInfoForm.loginName"></el-input>
+                </el-form-item>
+                <el-form-item label="初始密码" prop="pwd" class="is-required">
+                    <el-input  auto-complete="off" value="123" v-model="updateUserInfoForm.pwd"></el-input>
+                </el-form-item>
+                <el-form-item label="联系手机" >
+                    <el-input  auto-complete="off" v-model="updateUserInfoForm.tel"></el-input>
+                </el-form-item>
+                <el-form-item label="所属部门" prop="dept"  class="is-required">
+                    <el-select v-model="updateUserInfoForm.dept" placeholder="请选择部门...">
+                        <el-option label="业务室" value="4"></el-option>
+                        <el-option label="财务室" value="5"></el-option>
+                        <el-option label="综合科" value="6"></el-option>
+                        <el-option label="检验一室" value="1"></el-option>
+                        <el-option label="检验二室" value="2"></el-option>
+                        <el-option label="检验三室" value="3"></el-option>
+                    </el-select>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                    <el-button @click="dlg_isShowUpdateUserDlg=false;">取 消</el-button>
+                    <el-button type="primary" @click="updateUser()">确 定</el-button>
+            </span>
+        </el-dialog>
+        <el-dialog title="上传签名"  :visible.sync="dlg_isUploadSignDlg" size="small" :close-on-click-modal="dlg_modelCloseOption">
+            <el-upload
+                    class="upload-demo"
+                    action="https://jsonplaceholder.typicode.com/posts/"
+                    list-type="picture">
+                <el-button size="small" type="primary">点击上传</el-button>
+                <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+            </el-upload>
+            <span slot="footer" class="dialog-footer">
+                    <el-button @click="dlg_isShowUpdateUserDlg=false;">取 消</el-button>
+                    <el-button type="primary" @click="updateUser()">确 定</el-button>
+            </span>
+        </el-dialog>
+        <el-dialog title="确定删除吗？"  :visible.sync="dlg_isDeleteUserDlg" size="tiny" :close-on-click-modal="dlg_modelCloseOption">
+            <el-form :model="showUserInfoForm" :label-position="dlg_labelPosition" ref="showUserInfoForm" label-width="80px">
+            <el-form-item label="登录帐号">
+                <el-input  auto-complete="off" readonly="readonly" v-model="showUserInfoForm.loginName"></el-input>
+            </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                    <el-button @click="dlg_isDeleteUserDlg=false;">取 消</el-button>
+                    <el-button type="primary" @click="dlg_isDeleteUserDlg=false">确 定</el-button>
+            </span>
+        </el-dialog>
         <div class="userNav">
             <el-menu  class="el-menu-demo" mode="horizontal">
                 <el-menu-item index="1" @click="dlg_isShowAddUserDlg=true;"><i class="el-icon-plus"></i>添加用户</el-menu-item>
@@ -83,10 +169,10 @@
                         label="操作"
                         >
                     <template scope="scope">
-                        <el-button type="text" size="small">查看</el-button>
-                        <el-button type="text" size="small">修改</el-button>
-                        <el-button type="text" size="small">上传签名</el-button>
-                        <el-button type="text" size="small">删除</el-button>
+                        <el-button type="text" size="small" @click="dlg_isShowUserInfoDlg=true;">查看</el-button>
+                        <el-button type="text" size="small" @click="dlg_isShowUpdateUserDlg=true;">修改</el-button>
+                        <el-button type="text" size="small" @click="dlg_isUploadSignDlg=true;">上传签名</el-button>
+                        <el-button type="text" size="small" @click="dlg_isDeleteUserDlg=true;">删除</el-button>
                     </template>
                 </el-table-column>
             </el-table>
@@ -98,7 +184,13 @@
 export default{
     data(){
         return {
+            //窗口显示，关闭标志
             dlg_isShowAddUserDlg:false,
+            dlg_isShowUserInfoDlg:false,
+            dlg_isShowUpdateUserDlg:false,
+            dlg_isUploadSignDlg:false,
+            dlg_isDeleteUserDlg:false,
+
             dlg_modelCloseOption:false,
             dlg_labelPosition:'right',
             addUserForm:{
@@ -107,6 +199,20 @@ export default{
                 pwd:'',
                 tel:'',
                 dept:''
+            },
+            showUserInfoForm:{
+                name:'张三',
+                loginName:'zhangsan',
+                pwd:'123',
+                tel:'13912345678',
+                dept:'3'
+            },
+            updateUserInfoForm:{
+                name:'张三',
+                loginName:'zhangsan',
+                pwd:'123',
+                tel:'13912345678',
+                dept:'3'
             },
             addUser_rules:{
                 name:[
@@ -215,6 +321,16 @@ export default{
             this.$refs['addUserForm'].validate(function(valid){
                 if(valid){
                     this.dlg_isShowAddUserDlg = false;
+                }else{
+                    console.log('error submit!');
+                    return false;
+                }
+            }.bind(this));
+        },
+        updateUser(){
+            this.$refs['updateUserForm'].validate(function(valid){
+                if(valid){
+                    this.dlg_isShowUpdateUserDlg = false;
                 }else{
                     console.log('error submit!');
                     return false;
